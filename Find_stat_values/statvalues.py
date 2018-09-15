829d06f72d19522424c2a7eed2e3a609e4965b4c
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2018-09-14 23:42:29 -0400

    Add files via upload

--- Find_stat_values/statvalues.py
+++ Find_stat_values/statvalues.py
@@ -1,1 +1,8 @@
-
+import statistics as st
+import numpy as np
+if __name__ == "__main__":
+    numbers = [3,4,5,7,8,9,10]
+    print(st.mean(numbers))
+    print(st.median(numbers))
+    print(st.stdev(numbers))
+    print(st.variance(numbers))
